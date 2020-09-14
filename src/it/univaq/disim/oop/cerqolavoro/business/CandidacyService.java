44ea32f194d75d062198d5396251169c48ec6e5c
longobucco <lucavisconti2000@gmail.com>
2020-09-14 12:26:26 +0200

    Candidacy Service update

--- src/it/univaq/disim/oop/cerqolavoro/business/CandidacyService.java
+++ src/it/univaq/disim/oop/cerqolavoro/business/CandidacyService.java
@@ -1,16 +1,11 @@
 package it.univaq.disim.oop.cerqolavoro.business;
 
-import java.util.List;
-
-import it.univaq.disim.oop.cerqolavoro.business.BusinessException;
-import it.univaq.disim.oop.cerqolavoro.domain.Candidacy;
-
 public interface CandidacyService {
 
-	String[] findCandidacy(String email) throws CandidacyNotFoundException, BusinessException;
+	String[] findCandidacy(String email) throws BusinessException;
 
-	void createCandidacy(String email, String candidacyTitle) throws CandidacyNotFoundException, BusinessException;
+	void createCandidacy(String email, String candidacyTitle) throws BusinessException;
 
-	void deleteCandidacy(String email, String candidacyTitle) throws CandidacyNotFoundException, BusinessException;
+	void deleteCandidacy(String email, String candidacyTitle) throws BusinessException;
 	
 }
