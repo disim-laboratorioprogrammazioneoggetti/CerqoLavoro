edbf4b74466d79dc1bc108a88e65725898bd0939
longobucco <lucavisconti2000@gmail.com>
2020-09-15 22:35:34 +0200

    Skills Update

---
+++ src/it/univaq/disim/oop/cerqolavoro/domain/Skills.java
@@ -0,0 +1,33 @@
+package it.univaq.disim.oop.cerqolavoro.domain;
+
+public class Skills {
+
+	private String education;
+	private String category;
+	private String experience;
+	
+	public String getEducation() {
+		return education;
+	}
+
+	public void setEducation(String education) {
+		this.education = education;
+	}
+	
+	public String getSector() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	public String getExperience() {
+		return experience;
+	}
+
+	public void setExperience(String experience) {
+		this.experience = experience;
+	}
+
+}
